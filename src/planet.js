f9942676416d0618af121ab104bb2b886df56105
jdvolk <justin.d.volk@gmail.com>
2020-03-22 16:33:55 -0600

    add 2/3 ship.js

---
+++ src/planet.js
@@ -0,0 +1,18 @@
+class Planet {
+    constructor(name, shopName, canLand, shop, coordinates) {}
+//    - has a name
+//    - can have a different name
+//    - can have a shop
+//    - can have a different shop
+//    - can allow a ship to land
+//    - has coordinates
+//    - can have different coordinates
+//    - can calculate the distance between itself and another planet
+//    - can calculate the distance between itself and different planet
+//    - can refuel a ship
+//    - cannot send a ship to another planet if the ship is missing parts
+//    - cannot send a ship if it is has no fuel
+//    - cannot send a ship if it is has insufficient fuel
+//    - can send a ship and update the ship & planets
+}
+module.exports = Planet;
